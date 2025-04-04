90437d1d9a745ead18112ac6e56132dee4fc2b1f
Hellpes <alexrui_hpow@mail.ru>
2025-04-04 18:37:33 +0300

    add init page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
     return (
         <>
-            <h1 className="text-6xl text-emerald-500">Hello Init Project</h1>
+            <div className="flex min-h-screen min-w-screen items-center justify-center bg-[url(./assets/mountains.jpg)] bg-cover bg-no-repeat bg-origin-border">
+                <div className="h-[90vh] w-[70%] rounded-lg border-[1px] border-white/15 bg-white/5 p-12 backdrop-blur-sm"></div>
+            </div>
         </>
     );
 }
