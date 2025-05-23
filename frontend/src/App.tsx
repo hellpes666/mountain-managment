231835a42212e8df3c614eb8447b837c80001a34
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-05-23 20:41:54 +0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import Layout from './components/Layout/Layout';
+
+export const App = () => {
+	return (
+		<Layout>
+			<div className=""></div>
+		</Layout>
+	);
+};
---
+++ frontend/src/components/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,67 @@
+import { Calendar, Group, Home, Inbox, Mountain, Search, Settings, Table, User } from 'lucide-react';
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+// Menu items.
+const items = [
+	{
+		title: 'Home',
+		url: '#',
+		icon: Home,
+	},
+	{
+		title: 'Climbers',
+		url: '#',
+		icon: User,
+	},
+	{
+		title: 'Groups',
+		url: '#',
+		icon: Group,
+	},
+	{
+		title: 'Mountains',
+		url: '#',
+		icon: Mountain,
+	},
+	{
+		title: 'Export data',
+		url: '#',
+		icon: Table,
+	},
+];
+
+export function AppSidebar() {
+	return (
+		<Sidebar>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>Application</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<a href={item.url}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	);
+}
